25a444db0bfbc628334d699b79874ab13b8ac28d
Kristiyan Krustev <115258739+Kristradamus@users.noreply.github.com>
2025-10-30 13:35:29 +0200

    prucpruc

--- ReactProjects/ModalWindow/src/App.jsx
+++ ReactProjects/ModalWindow/src/App.jsx
@@ -1,5 +1,29 @@
 import './App.css'
+import {useState} from "react";
 
 export default function modalWindow(){
+  const [showWindow, setShowWindow] = useState(false);
 
+  const showWindowFunc = function(){
+    setShowWindow(true);
+  }
+
+return(
+    <>
+      <div className="buttonsBox">
+        <button className="button1" onClick={showWindowFunc}>Bla Bla Bla</button>
+        <button className="button2" onClick={showWindowFunc}>Ble Ble Ble</button>
+        <button className="button3" onClick={showWindowFunc}>Bli Bli Bli</button>
+      </div>
+      <div className={`modalWindowBox ${showWindow ? "show" : ""}`}>
+        <button className="exitBtn">x</button>
+        <h1 className="modalTitle">El salsa la pikante</h1>
+        <p className="modalText">Lingaguli guli guli lingangu lingangu, 
+            Lingaguli guli guli lingangu lingangu, 
+            Lingaguli guli guli lingangu lingangu, 
+            Lingaguli guli guli lingangu lingangu</p>
+      </div>
+      <div className={`overlay ${showWindow ? "show" : ""}`}></div>
+    </>
+)
 }
